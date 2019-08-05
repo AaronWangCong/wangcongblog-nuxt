9fb271d6a7d0d56dcae91e6d3094765416b0dce8
wangcong <13997592990@163.com>
2019-08-05 09:12:45 +0800

    wc

---
+++ pages/myworld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="myworld-wrap">
+  </div>
+</template>
+<script>
+  export default {
+    name: '',
+    components: {
+    },
+    props:{
+    },
+    layout:'',
+    data() {
+      return {
+      }
+    },
+    computed: {
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+    },
+  }
+</script>
+<style lang="scss">
+.myworld-wrap{
+}
+</style>
