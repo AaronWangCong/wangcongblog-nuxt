41e5cd4d9401d9486cd6b5d1fae8875e5bdfc7c4
wangcong <13997592990@163.com>
2020-04-25 23:30:47 +0800

    wc

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,17 @@
     <indexVideo @onClick="onClick" />
     <div v-if="isShowText" class="index-wrap-content">
       <span :class="isAnimate ? 'animated rubberBand infinite' : ''" class="index-logo" @mouseenter="mouseenter" @mouseleave="mouseleave"><img src="../assets/images/favicon.jpg" alt=""></span>
-      <h2 class="index-title animated rollIn fast">Aaron汪聪</h2>
-      <div class="index-text animated rollIn fast">
-        <p>乙丑岁赴试并州，道逢捕雁者云：“今旦获一雁，杀之矣。其脱网者悲鸣不能去，竟自投于地而死。”予因买得之，葬之汾水之上，垒石为识，号曰“雁丘”。同行者多为赋诗，予亦有《雁丘词》。旧所作无宫商，今改定之。</p>
-        <p>问世间，情为何物，直教生死相许？</p>
-        <p>天南地北双飞客，老翅几回寒暑。</p>
-        <p>欢乐趣，离别苦，就中更有痴儿女。</p>
-        <p>君应有语：渺万里层云，千山暮雪，只影向谁去？</p>
-        <p>横汾路，寂寞当年箫鼓，荒烟依旧平楚。</p>
-        <p>招魂楚些何嗟及，山鬼暗啼风雨。</p>
-        <p>天也妒，未信与，莺儿燕子俱黄土。</p>
-        <p>千秋万古，为留待骚人，狂歌痛饮，来访雁丘处。</p>
+      <h2 class="index-title animated lightSpeedIn fast">Aaron汪聪</h2>
+      <div class="index-text">
+        <p class="animated slideInLeft fast">乙丑岁赴试并州，道逢捕雁者云：“今旦获一雁，杀之矣。其脱网者悲鸣不能去，竟自投于地而死。”予因买得之，葬之汾水之上，垒石为识，号曰“雁丘”。同行者多为赋诗，予亦有《雁丘词》。旧所作无宫商，今改定之。</p>
+        <p class="animated lightSpeedIn fast">问世间，情为何物，直教生死相许？</p>
+        <p class="animated slideInLeft fast">天南地北双飞客，老翅几回寒暑。</p>
+        <p class="animated lightSpeedIn fast">欢乐趣，离别苦，就中更有痴儿女。</p>
+        <p class="animated slideInLeft fast">君应有语：渺万里层云，千山暮雪，只影向谁去？</p>
+        <p class="animated lightSpeedIn fast">横汾路，寂寞当年箫鼓，荒烟依旧平楚。</p>
+        <p class="animated slideInLeft fast">招魂楚些何嗟及，山鬼暗啼风雨。</p>
+        <p class="animated lightSpeedIn fast">天也妒，未信与，莺儿燕子俱黄土。</p>
+        <p class="animated slideInLeft fast">千秋万古，为留待骚人，狂歌痛饮，来访雁丘处。</p>
       </div>
 
       <nuxt-link to="/home" class="homeBtn animated jackInTheBox fast">网站建设中,进入主站</nuxt-link>
